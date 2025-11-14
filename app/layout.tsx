1d2f2d757f9ba77734f7c6bb6e55b8ece9593d57
teyorkk <theomacatienza@gmail.com>
2025-11-15 01:17:22 +0800

    added speed insights by vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { SessionProvider } from "@/components/session-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,8 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
         <SessionProvider>
+          <SpeedInsights />
           {children}
         </SessionProvider>
       </body>
